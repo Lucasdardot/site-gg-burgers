07073303f36fa3ebb2bca0d89fde84bd1dbffeae
Lucasdardot <lucasdardot10@gmail.com>
2024-10-28 15:55:51 -0300

    ajustes ao adicionar o hambúrger

--- src/components/Carrinho.jsx
+++ src/components/Carrinho.jsx
@@ -1,4 +1,4 @@
-import "./Carrinho.css"
+import "./Carrinho.css";
 import { IoCloseSharp } from "react-icons/io5";
 import { FaRegTrashCan } from "react-icons/fa6";
 
@@ -7,38 +7,32 @@ function Carrinho({ fecharCarrinho, itensCarrinho }) {
         <div className="carrinho-principal">
             <div className="carrinho-header">
                 <h2>CARRINHO</h2>
-                <button className="btn-fechar" onClick={fecharCarrinho}><IoCloseSharp />
-
-                </button>
+                <button className="btn-fechar" onClick={fecharCarrinho}><IoCloseSharp /></button>
             </div>
             <div className="carrinhoConteudo">
-                {itensCarrinho.length === 0 ?
-                    (<p className="carrinho-vazio">seu carrinho está vazio</p>)
-                    : (itensCarrinho.map((item) => {
+                {itensCarrinho.length === 0 ? (
+                    <p className="carrinho-vazio">Seu carrinho está vazio</p>
+                ) : (
+                    itensCarrinho.map((item) => (
                         <div className="itemCarrinho" key={item.id}>
                             <div>
                                 <h3>{item.nome}</h3>
-                                <p>quantidade: {item.quantidade}</p>
+                                <p>Quantidade: {item.quantidade}</p>
                                 <p>R$: {(item.preco * item.quantidade).toFixed(2)}</p>
                             </div>
-                            <div><button className="btn-remover"><FaRegTrashCan /></button></div>
+                            <div>
+                                <button className="btn-remover"><FaRegTrashCan /></button>
+                            </div>
                         </div>
-                    })
-
-                    )
-                }
-
-
+                    ))
+                )}
             </div>
             <div className="carrinhoFooter">
                 <h2>Total: R$100,00</h2>
                 <button className="finalizarCompra">Finalizar compra</button>
             </div>
-
-
         </div>
-    )
-
+    );
 }
 
-export default Carrinho
+export default Carrinho;
